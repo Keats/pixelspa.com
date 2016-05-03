7e2b0d187149e8b9cbb5b8c03141695b161e159d
Vincent Prouillet <vincent@wearewizards.io>
2016-05-03 12:41:18 +0100

    New deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # Generate the website
-cd pixelspa && hugo
+hugo
 
-# Generated website is in pixelspa/public directory
-# Let's move that to S3
-npm run publish
+# Add to git
+git add -A
+
+# Commit
+git commit -m "Site updated `date`"
+
+# Push to github
+git push origin master
+git subtree push --prefix=public git@github.com:WeAreWizards/pixelspa.com.git gh-pages
